e4600b28fe4cb92c9cb44643e2f19693693d0f4a
Pablo Estrella <almendaris123@gmail.com>
2021-09-27 22:01:15 -0500

    se quitó plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   // },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/sw.client.js'
+    // '~/plugins/sw.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
