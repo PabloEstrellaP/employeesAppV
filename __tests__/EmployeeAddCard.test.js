1f6302c90eb9839f8577d8fb0843d8294a1db92a
Pablo Estrella <almendaris123@gmail.com>
2021-11-02 17:37:00 -0600

    Se agregó el test

---
+++ __tests__/EmployeeAddCard.test.js
@@ -0,0 +1,11 @@
+
+
+import { mount } from '@vue/test-utils'
+import EmployeeAddCard from '../components/EmployeeAddCard.vue' // ../my-component.vue file
+
+describe('MyComponent', () => {
+  it('Render correctly', () => {
+    const wrapper = mount(EmployeeAddCard)
+    expect(wrapper.vm.$el).toMatchSnapshot()
+  })
+})
