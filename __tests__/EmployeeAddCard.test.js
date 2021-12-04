7685fbb1b4a7a15e9f9f43b41604a191a772e723
Edgar Lopez <edgarlopezmgs@live.com>
2021-12-03 23:16:34 -0600

    fix: build test

--- __tests__/EmployeeAddCard.test.js
+++ __tests__/EmployeeAddCard.test.js
@@ -1,11 +1,9 @@
-import Vuetify from 'vuetify'
-
 import { mount } from '@vue/test-utils'
 import EmployeeAddCard from '../components/EmployeeAddCard.vue' // ../my-component.vue file
 
 describe('MyComponent', () => {
   it('Render correctly', () => {
     const wrapper = mount(EmployeeAddCard)
-    expect(wrapper.menssage).toBe('Hello World')
+    expect(wrapper.vm.$el).toMatchSnapshot()
   })
 })
